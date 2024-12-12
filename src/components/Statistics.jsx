70da9255c0d03b160179afd948ba72f7a465cee8
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2024-12-12 20:39:01 +0400

    Add dashboard statistics chart without logic

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,16 +1,32 @@
-import { imageBaseUrl } from "@/components/Sidebar.jsx";
-import {
-  Bar,
-  CartesianGrid,
-  Legend,
-  Tooltip,
-  XAxis,
-  YAxis,
-  BarChart,
-} from "recharts";
+import React from 'react'
+import {Cell, Pie, PieChart} from "recharts";
 
+
+// all implementation later
 const Statistics = () => {
-  return <div>Lorem ipsum dolor.</div>;
-};
+  const data = [
+    { name: 'Entertainment', value: 30, fill: 'rgba(52, 60, 106, 1)' },
+    { name: 'Investment', value: 20, fill: 'rgba(250, 0, 255, 1)' },
+    { name: 'Others', value: 35, fill: 'rgba(24, 20, 243, 1)' },
+    { name: 'Bill Expense', value: 15, fill: 'rgba(252, 121, 0, 1)' },
 
-export default Statistics;
+  ];
+
+    return (
+        <div className='flex-center w-full'>
+         <PieChart width={300} height={300} >
+           <Pie data={data} dataKey="value" nameKey="name" outerRadius={135} startAngle={40} endAngle={400}
+           >
+             {data.map((entry, index) => (
+                 <Cell key={`cell-${index}`} fill={entry.fill || '#8884d8'} />
+             ))}
+           </Pie>
+         </PieChart>
+
+        </div>
+    )
+}
+export default Statistics
+
+
+
